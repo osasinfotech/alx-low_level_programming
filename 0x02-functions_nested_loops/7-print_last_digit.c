e097d7ff2662f50219a4e191c0d62d3b0adcef53
osasinfotech <airdropinfo@gmail.com>
2022-09-14 22:20:28 -0700

    function 2nd day

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * print_last_digit - function that prints the last digit 
+ * print_last_digit - function that prints the last digit
  *@n: integer number
 
- * Return: Always 0
+ * Return: last
  */
 int print_last_digit(int n)
 {
 	int last;
-	last = n % 10 + '0';
+
+	last = (n % 10) + '0';
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
 
 	return (last);
 }
